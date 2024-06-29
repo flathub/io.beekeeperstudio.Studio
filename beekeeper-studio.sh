b803d02f696f04a6b4f9ee725ca0af50b33e98b4
proletarius101 <54175165+proletarius101@users.noreply.github.com>
2024-06-29 18:28:39 +0800

    feat: add wayland support (#31)

--- beekeeper-studio.sh
+++ beekeeper-studio.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
 set -oue pipefail
-
-export FLATPAK_ID="${FLATPAK_ID:-io.beekeeperstudio.Studio}"
 export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
 
-exec zypak-wrapper /app/beekeeper-studio/beekeeper-studio-bin $@
+FLAGS=''
+if [[ $XDG_SESSION_TYPE == "wayland" && -e "$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY" ]]
+then
+    FLAGS="$FLAGS --enable-wayland-ime --ozone-platform-hint=wayland --enable-features=WaylandWindowDecorations --gtk-version=4"
+    if  [ -c /dev/nvidia0 ]
+    then
+        FLAGS="$FLAGS --disable-gpu-sandbox"
+    fi
+fi
+
+exec zypak-wrapper /app/beekeeper-studio/beekeeper-studio-bin $FLAGS "$@"
